48b713ec461e78bb7265ba5439194b570686aff6
Niels Frerichs <n.frerichs83@gmail.com>
2021-03-15 23:05:39 +0100

    updates

--- backend/src/main/java/App.java
+++ backend/src/main/java/App.java
@@ -1,5 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args){
+        logger.info("logging working");
         System.out.println("yolo");
     }
 }
